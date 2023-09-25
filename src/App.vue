1cde5f18dfbc7ce5a2ccb17957b517ed6ea33ca8
Dominika <dominika-biedulska@tlen.pl>
2023-09-25 12:40:58 +0200

    feat: add component Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <Home />
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
 
-@Options({
+export default {
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+    Home
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="flex justify-center mt-16 text-8xl text-blue-800 uppercase underline font-lucky space-x-10">
+      <div v-for="(x, index) in 'wisielec'" :key="index">
+        {{ x }}
+      </div>
+    </div>
+    <div class="ml-60 my-40">
+      <transition name="text" appear enter-active-class="transition-all duration-1000 ease-in"
+        enter-from-class="opacity-0 -translate-x-[500px]">
+        <img src="@/img/hangman.png" />
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HomePage',
+  setup() {
+    const name = 'wisielec'
+    return {
+      name
+    }
+  }
+}
+</script>
