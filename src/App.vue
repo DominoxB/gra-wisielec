f5e760ba5c60a77bedf4b675081b0b7a33590311
Dominika <dominika-biedulska@tlen.pl>
2023-09-25 13:22:39 +0200

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Home />
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
-import Home from './components/Home.vue';
+import Home from './components/Home.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Home
+    Home,
+    Footer
   }
 }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="flex justify-center mt-16 text-8xl text-blue-800 uppercase underline font-lucky space-x-10">
+  <div class="font-lucky">
+    <div class="flex justify-center mt-16 text-8xl text-blue-800 uppercase underline space-x-10">
       <div v-for="(x, index) in 'wisielec'" :key="index">
         {{ x }}
       </div>
@@ -8,16 +8,16 @@
     <div class="grid grid-cols-2 justify-center items-center mt-20">
       <transition name="text" appear enter-active-class="transition-all duration-1000 ease-in"
         enter-from-class="opacity-0 -translate-x-[500px]">
-        <img src="@/img/hangman.png" class="mx-auto"/>
+        <img src="@/img/hangman.png" class="mx-auto" />
       </transition>
       <transition name="text" appear enter-active-class="transition-all duration-1000 ease-in"
         enter-from-class="opacity-0 translate-x-[500px]">
-    <div class="font-lucky text-6xl mx-auto text-center">
-      <span>start</span>
-      <img src="@/img/arrow.png" class="mt-8 cursor-pointer" />
+        <div class="text-6xl mx-auto text-center">
+          <span>start</span>
+          <img src="@/img/arrow.png" class="mt-8 cursor-pointer" />
+        </div>
+      </transition>
     </div>
-  </transition>
-  </div>
   </div>
 </template>
 
