f1aae997aeccdb0dec2f0a5c7f19a074258b77b9
Dominika <dominika-biedulska@tlen.pl>
2023-09-26 10:09:36 +0200

    feat: prepare responsive design

--- src/components/IntroContent.vue
+++ src/components/IntroContent.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="grid grid-cols-2 justify-center items-center mt-20 font-lucky">
+  <div class="md:grid md:grid-cols-2 justify-center items-center mt-12 md:mt-20 font-lucky">
     <transition name="text" appear enter-active-class="transition-all duration-1000 ease-in"
       enter-from-class="opacity-0 -translate-x-[500px]">
-      <img src="@/img/hangman.png" class="mx-auto" />
+      <img src="@/img/hangman.png" class="mx-auto mb-10 md:mb-0" />
     </transition>
     <transition name="text" appear enter-active-class="transition-all duration-1000 ease-in"
       enter-from-class="opacity-0 translate-x-[500px]">
       <div class="text-6xl mx-auto text-center">
         <span>start</span>
-        <img src="@/img/arrow.png" class="mt-8 cursor-pointer" @click="$emit('clickPlay')"/>
+        <img src="@/img/arrow.png" class="mt-8 cursor-pointer mx-auto" @click="$emit('clickPlay')"/>
       </div>
     </transition>
   </div>
