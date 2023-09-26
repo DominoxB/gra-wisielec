081b22be4b62299e30059acd690fc5ee64812d02
Dominika <dominika-biedulska@tlen.pl>
2023-09-26 15:01:18 +0200

    feat: add space

--- src/components/IntroContent.vue
+++ src/components/IntroContent.vue
@@ -8,7 +8,7 @@
       enter-from-class="opacity-0 translate-x-[500px]">
       <div class="text-6xl mx-auto text-center">
         <span>start</span>
-        <img src="@/img/arrow.png" class="mt-8 cursor-pointer mx-auto" @click="$emit('clickPlay')"/>
+        <img src="@/img/arrow.png" class="mt-8 cursor-pointer mx-auto" @click="$emit('clickPlay')" />
       </div>
     </transition>
   </div>
