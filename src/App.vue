3711f19fc0f66902f6fe7eb65b25b1e6bad77a22
Dominika <dominika-biedulska@tlen.pl>
2023-09-27 17:06:16 +0200

    feat: add ts element

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,15 @@
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue'
 import Home from './components/Home.vue'
 import Footer from './components/Footer.vue'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
     Home,
     Footer
   }
-}
+})
 </script>
